65fc97e13e18e19ea19b18d89c63fd05ffd0aaab
Roby Setiawan <39855052+robyiset@users.noreply.github.com>
2023-09-24 15:08:51 +0700

    add .net 6.0 app files

---
+++ Starting-Project/Controllers/program_detailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Starting_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class program_detailsController : ControllerBase
+    {
+
+    }
+}
